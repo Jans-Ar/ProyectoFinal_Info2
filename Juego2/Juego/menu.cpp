c43849d1005469da0895d10745ebc21c5d87f38a
Jans-Ar <juans.galeano@udea.edu.co>
2024-12-02 03:24:15 +0000

    Se Cambio el fonod del Menu y la disposicion de los botones

--- Juego2/Juego/menu.cpp
+++ Juego2/Juego/menu.cpp
@@ -7,13 +7,17 @@ Menu::Menu(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    /*// Ajustar la imagen de fondo al tamaño del widget
-    this->setStyleSheet("Menu {"
-                        "background-image: url(:/Menu.jpg);"
-                        "background-repeat: no-repeat;"
-                        "background-position: center;"
-                        "background-size: cover;" // Escala la imagen para llenar el widget
-                        "}");*/
+    // Crear un QLabel para mostrar la imagen de fondo
+    QLabel *backgroundLabel = new QLabel(this);
+    backgroundLabel->setGeometry(0, 0, 600, 300); // Ajustar al tamaño del widget
+    QPixmap pixmap(":/Menu.jpg");
+
+    // Escalar la imagen al tamaño del QLabel (600x300 px)
+    backgroundLabel->setPixmap(pixmap.scaled(600, 300, Qt::KeepAspectRatioByExpanding, Qt::SmoothTransformation));
+    backgroundLabel->setScaledContents(true);
+
+    // Enviar el QLabel al fondo
+    backgroundLabel->lower();
 
     connect(ui->btnN1, &QPushButton::clicked, this, &Menu::abrirNivel1);
     connect(ui->btnN2, &QPushButton::clicked, this, &Menu::abrirNivel2);
